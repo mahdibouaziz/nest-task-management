257d7089d187a178a6e427d43e8e6d740a128043
Mahdi <mahdibouaziz@hotmail.fr>
2021-04-20 03:21:53 +0100

    added the ValidationPipe to the global of our App + applyit to the create task dto

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,11 @@
+import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  //Tell NestJs to use the Validation pipe in the entire application
+  app.useGlobalPipes(new ValidationPipe());
   await app.listen(3000);
 }
 bootstrap();
