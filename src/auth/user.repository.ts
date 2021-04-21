210ee0cba2e754715953dd6ebda9a36797821c4c
Mahdi Bouaziz <mahdibouaziz@hotmail.fr>
2021-04-21 15:50:52 +0100

    basic signUp without hashing the password and without validation rules

--- src/auth/user.repository.ts
+++ src/auth/user.repository.ts
@@ -1,5 +1,17 @@
 import { EntityRepository, Repository } from 'typeorm';
+import { AuthCredentialsDto } from './dto/auth-credentials.dto';
 import { User } from './user.entity';
 
 @EntityRepository(User)
-export class UserRepository extends Repository<User> {}
+export class UserRepository extends Repository<User> {
+  async signUp(authCredentialsDto: AuthCredentialsDto): Promise<void> {
+    //get the credentials
+    const { username, password } = authCredentialsDto;
+
+    //Create a new user
+    const user = this.create({ username, password });
+
+    //save it to the database
+    await this.save(user);
+  }
+}
